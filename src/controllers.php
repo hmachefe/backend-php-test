40758edb78ffd69850682206aa52cc3e135df36d
Isabelle Gachon <hugo.machefer@cpexterne.org>
2016-08-14 23:03:56 +0200

    TASK-5: manage pagination by PagerFantaExtension and templating

--- src/controllers.php
+++ src/controllers.php
@@ -3,12 +3,17 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+use FranMoreno\Silex\Twig\PagerfantaExtension;
+use Pagerfanta\Pagerfanta;
+use Pagerfanta\Adapter\ArrayAdapter;
+
 $app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
     $twig->addGlobal('user', $app['session']->get('user'));
 
     return $twig;
 }));
 
+$app['twig']->addExtension(new PagerfantaExtension($app));
 
 $app->get('/', function () use ($app) {
     return $app['twig']->render('index.html', [
@@ -41,7 +46,7 @@ $app->get('/logout', function () use ($app) {
 });
 
 
-$app->get('/todo/{id}', function ($id) use ($app) {
+$app->get('/todo/{id}', function ($id, Request $request) use ($app) {
     if (null === $user = $app['session']->get('user')) {
         return $app->redirect('/login');
     }
@@ -57,23 +62,34 @@ $app->get('/todo/{id}', function ($id) use ($app) {
         $sql = "SELECT * FROM todos WHERE user_id = '${user['id']}'";
         $todos = $app['db']->fetchAll($sql);
 
-        return $app['twig']->render('todos.html', [
-            'todos' => $todos,
-        ]);
+        $adapter = new ArrayAdapter($todos);
+        $pagerfanta = new Pagerfanta($adapter);
+        $pagerfanta->setMaxPerPage(10);
+        $pagerfanta->setCurrentPage($request->query->get('page', 1));
+
+        return $app['twig']->render('todos.paginated.html', array(
+            'pager' => $pagerfanta
+        ));
+
     }
 })
 ->value('id', null);
 
 
 $app->get('/todo/{id}/json', function ($id) use ($app) {
+
     $user = $app['session']->get('user');
+
     if ($id){
         $sql = "SELECT * FROM todos WHERE id = '$id'";
         $todo = $app['db']->fetchAssoc($sql);
+
           $jsonData = json_encode($todo);
+
           $headers = array(
                 'Content-Type' => 'application/json'
           );
+
           $response = new Response($jsonData, 200, $headers);
           return $response;
     }
@@ -88,7 +104,6 @@ $app->post('/todo/add', function (Request $request) use ($app) {
 
     $user_id = $user['id'];
     $description = $request->get('description');
-
     if ($description != '' && (strlen(trim($description)) != 0)) {
     $sql = "INSERT INTO todos (user_id, description) VALUES ('$user_id', '$description')";
     $app['db']->executeUpdate($sql);
@@ -96,6 +111,7 @@ $app->post('/todo/add', function (Request $request) use ($app) {
         // #TODO warn users that empty strings are not allowed
         error_log('empty description is not granted. Please fill in with relevant content');
     }
+
     return $app->redirect('/todo');
 });
 
