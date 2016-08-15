f937863ab583a485326c4ba0d1471788a20e2076
Isabelle Gachon <hugo.machefer@cpexterne.org>
2016-08-15 13:13:51 +0200

    TASK-6: gain more control by ORM on sql breakdown

--- src/UserDao.php
+++ src/UserDao.php
@@ -20,47 +20,82 @@ class UserDao
 	public function findUser($username, $password)
 	{
         $sql = "SELECT * FROM users WHERE username = '$username' and password = '$password'";
-        $user = $this->getDb()->fetchAssoc($sql);
+        try {
+        	$user = $this->getDb()->fetchAssoc($sql);
+        } catch (Doctrine\DBAL\DBALException $e) {
+        	$error_log( $sql->errorInfo() );
+        	$error_log( $sql->errorCode() );
+        }
         return $user;
 	}
 
 	public function findDescriptionById($id)
 	{
         $sql = "SELECT * FROM todos WHERE id = '$id'";
-        $todo = $this->getDb()->fetchAssoc($sql);
+        try {
+        	$todo = $this->getDb()->fetchAssoc($sql);
+        } catch (Doctrine\DBAL\DBALException $e) {
+        	$error_log( $sql->errorInfo() );
+        	$error_log( $sql->errorCode() );
+        }
         return $todo;
 	}
 
 	public function findDescriptionByUser($user)
 	{
         $sql = "SELECT * FROM todos WHERE user_id = '${user['id']}'";
-        $todos = $this->getDb()->fetchAll($sql);
+        try {
+        	$todos = $this->getDb()->fetchAll($sql);
+        } catch (Doctrine\DBAL\DBALException $e) {
+        	$error_log( $sql->errorInfo() );
+        	$error_log( $sql->errorCode() );
+        }
         return $todos;
 	}
 
 	public function getDescription($id)
 	{
 	    $sql = "SELECT * FROM todos WHERE id = '$id'";
-	    $todo = $this->getDb()->fetchAssoc($sql);
+        try {
+        	$todo = $this->getDb()->fetchAssoc($sql);
+        } catch (Doctrine\DBAL\DBALException $e) {
+        	$error_log( $sql->errorInfo() );
+        	$error_log( $sql->errorCode() );
+        }
 	    return $todo["description"];
 	}
 
 	public function deleteDescription($id)
 	{
 	    $sql = "DELETE FROM todos WHERE id = '$id'";
-	    $this->getDb()->executeUpdate($sql);
+        try {
+        	$this->getDb()->executeUpdate($sql);
+        } catch (Doctrine\DBAL\DBALException $e) {
+        	$error_log( $sql->errorInfo() );
+        	$error_log( $sql->errorCode() );
+        }
 	}
 
 	public function markDescriptionAsCompleted($id)
 	{
 	    $sql = "UPDATE todos SET completed = 1 WHERE id = '$id'";
-	    $this->getDb()->executeUpdate($sql);
+        try {
+        	$this->getDb()->executeUpdate($sql);
+        } catch (Doctrine\DBAL\DBALException $e) {
+        	$error_log( $sql->errorInfo() );
+        	$error_log( $sql->errorCode() );
+        }
 	}
 
 	public function addDescription($user_id, $description, $completed)
 	{
 	    $sql = "INSERT INTO todos (user_id, description, completed) VALUES ('$user_id', '$description', '$completed')";
-	    $this->getDb()->executeUpdate($sql);
+        try {
+        	$this->getDb()->executeUpdate($sql);
+        } catch (Doctrine\DBAL\DBALException $e) {
+        	$error_log( $sql->errorInfo() );
+        	$error_log( $sql->errorCode() );
+        }
 	}
 
 }
